29d8a3a53cfafd7c0aaf443d0600f42f265f097e
akshatagawas <68987251+akshatagawas@users.noreply.github.com>
2024-06-13 11:28:37 -0400

    Time: 69 ms (6.93%) | Memory: 16.6 MB (40.00%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -1,8 +1,29 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats = sorted(seats)
-        students = sorted(students)
+        # seats = sorted(seats)
+        # students = sorted(students)
+        # res = 0
+        # for i in range(len(seats)):
+        #     res += abs(seats[i] - students[i])
+        # return res
+        max_index = max(max(seats), max(students)) + 1
+        count_seats = [0] * max_index
+        count_students = [0] * max_index
+        for seat in seats:
+            count_seats[seat] += 1
+        for student in students:
+            count_students[student] += 1
+        i,j = 0, 0
         res = 0
-        for i in range(len(seats)):
-            res += abs(seats[i] - students[i])
+        remain = len(students)
+        while remain:
+            if count_seats[i] == 0:
+                i += 1
+            if count_students[j] == 0:
+                j += 1
+            if count_seats[i] and count_students[j]:
+                res += abs(i - j)
+                count_seats[i] -= 1
+                count_students[j] -= 1
+                remain -= 1
         return res
